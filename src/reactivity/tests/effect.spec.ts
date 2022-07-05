532c094aeb5e4822554f55e74483992719e55ae3
wanqiang <wanqiang@ebupt.com>
2022-07-05 15:08:28 +0800

    featt实现effect 返回runner

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -13,10 +13,21 @@ describe ("effect", () => {
             nextAge = user.age + 1
         })
         expect(nextAge).toBe(11)
-
         user.age++;
         expect(nextAge).toBe(12)
+    })
+    it ('should return runner when call effect', () => {
+        // effect(fn) -> function(runner) -> fn -> return
+        // 执行effect 可以拿到内部fn的返回值
+        let foo = 10 
+        const runner = effect(() =>{
+            foo++
+            return 'foo'
+        })
 
-
+        expect(foo).toBe(11)
+        const r = runner()
+        expect(foo).toBe(12)
+        expect(r).toBe('foo')
     })
 })
