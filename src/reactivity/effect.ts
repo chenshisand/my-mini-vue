e81ad5cbdbe6990118bf2f9b58a064fd8939bbb7
wanqiang <wanqiang@ebupt.com>
2022-07-12 17:20:34 +0800

    feat:实现readonly功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,77 +1,76 @@
-import { extend } from "../shared"
+import { extend } from "../shared";
 
 class ReactiveEffect {
-    private _fn: any
-    public scheduler: Function | undefined 
-    deps = []
-    active = true
-    onStop?: () => void
-    constructor(fn, scheduler?: Function) {
-        this._fn = fn
-        this.scheduler = scheduler
+  private _fn: any;
+  public scheduler: Function | undefined;
+  deps = [];
+  active = true;
+  onStop?: () => void;
+  constructor(fn, scheduler?: Function) {
+    this._fn = fn;
+    this.scheduler = scheduler;
+  }
+  run() {
+    activeEffect = this;
+    return this._fn();
+  }
+  stop() {
+    if (this.active) {
+      clearnupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
     }
-    run () {
-        activeEffect = this
-        return  this._fn()
-    }
-    stop () {
-        if(this.active) {
-            clearnupEffect(this)
-            if(this.onStop) {
-                this.onStop()
-            }
-            this.active = false
-        }
-    }
+  }
 }
-function clearnupEffect (effect) {
-    effect.deps.forEach((dep: any) => {
-        dep.delete(effect)
-    })
-}
-
-const targetMap = new Map()
-export function track (target, key) {
-    let depsMap = targetMap.get(target)
-    if (!depsMap) {
-        depsMap = new Map()
-        targetMap.set(target, depsMap)
-    }
-
-    let dep = depsMap.get(key)
-    if (!dep) {
-        dep = new Set()
-        depsMap.set(key, dep)
-
-    }
-    if (!activeEffect) return
-    dep.add(activeEffect)
-    activeEffect.deps.push(dep)
+function clearnupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
 }
 
-export function trigger (target, key) {
-    let depsMap = targetMap.get(target)
-    let dep = depsMap.get(key)
+const targetMap = new Map();
+export function track(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
 
-    for(const effect of dep) {
-        if(effect.scheduler) {
-            effect.scheduler()
-        } else {
-            effect.run()
-        }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+  if (!activeEffect) return;
+  dep.add(activeEffect);
+  activeEffect.deps.push(dep);
+}
+
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+
+  let dep = depsMap.get(key);
+
+  for (const effect of dep) {
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
     }
-
+  }
 }
 let activeEffect;
 export function effect(fn, options: any = {}) {
-    // fn
-    const _effect = new ReactiveEffect(fn, options.scheduler)
-    extend(_effect, options)
-    _effect.run()
-    const runner: any = _effect.run.bind(_effect)
-    runner.effect = _effect
-    return runner
+  // fn
+  const _effect = new ReactiveEffect(fn, options.scheduler);
+  extend(_effect, options);
+  _effect.run();
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
 }
-export function stop (runner) {
-    runner.effect.stop()
+export function stop(runner) {
+  runner.effect.stop();
 }
