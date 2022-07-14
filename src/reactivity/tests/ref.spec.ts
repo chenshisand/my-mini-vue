d0edb134536b69ed7fea272cb6712ac511c03302
chenshisand <chenshisand@163.com>
2022-07-15 01:30:23 +0800

    feat:实现isRef和unRef

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from "../effect";
-import { ref } from "../ref";
+import { reactive } from "../reactive";
+import { isRef, ref, unRef } from "../ref";
 
 describe("ref", () => {
   it("happy path", () => {
@@ -34,5 +35,17 @@ describe("ref", () => {
     expect(dumy).toBe(1);
     a.value.count = 2;
     expect(dumy).toBe(2);
+  });
+  it("isRef", () => {
+    const a = ref(1);
+    const user = reactive({ foo: 1 });
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(user)).toBe(false);
+  });
+  it("unRef", () => {
+    const a = ref(2);
+    expect(unRef(a)).toBe(2);
+    expect(unRef(1)).toBe(1);
   });
 });
