1e4be182cf5911cb302964b8d95ae3f1aa3032d7
wanqiang <wanqiang@ebupt.com>
2022-07-15 17:53:39 +0800

    feat:computed初始搭建

---
+++ src/runtIme-core/render.ts
@@ -0,0 +1,21 @@
+import { createComponentInstance, setupComponent } from "./component";
+
+export function render(vnode, container) {
+  // path
+  path(vnode, container);
+}
+function path(vnode, container) {
+  processComponent(vnode, container);
+}
+function processComponent(vnode: any, container: any) {
+  mountComponent(vnode, container);
+}
+function mountComponent(vnode: any, container) {
+  const instance = createComponentInstance(vnode);
+  setupComponent(instance);
+  setupRenderEffect(instance, container);
+}
+function setupRenderEffect(instance, container) {
+  const subTree = instance.render();
+  path(subTree, container);
+}
