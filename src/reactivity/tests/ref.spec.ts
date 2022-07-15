fc4ac99ced5f092c9803806fe262f59d7d53c407
wanqiang <wanqiang@ebupt.com>
2022-07-15 13:57:59 +0800

    feat:实现proxyRefs功能

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from "../effect";
 import { reactive } from "../reactive";
-import { isRef, ref, unRef } from "../ref";
+import { isRef, proxyRefs, ref, unRef } from "../ref";
 
 describe("ref", () => {
   it("happy path", () => {
@@ -47,5 +47,23 @@ describe("ref", () => {
     const a = ref(2);
     expect(unRef(a)).toBe(2);
     expect(unRef(1)).toBe(1);
+  });
+  it("proxyRefs", () => {
+    const user = {
+      age: ref(10),
+      name: "haha",
+    };
+    const proxyUser = proxyRefs(user);
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    expect(proxyUser.name).toBe("haha");
+
+    proxyUser.age = 20;
+    expect(proxyUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
+
+    proxyUser.age = ref(15);
+    expect(proxyUser.age).toBe(15);
+    expect(user.age.value).toBe(15);
   });
 });
