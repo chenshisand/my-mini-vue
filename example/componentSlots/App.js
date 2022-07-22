521190c563382051bac6a6513aa01a230773e42a
wanqiang <wanqiang@ebupt.com>
2022-07-22 15:28:36 +0800

    feat:实现组件slots功能

---
+++ example/componentSlots/App.js
@@ -0,0 +1,20 @@
+import { h } from "../../lib/guide-mini-vue.esm.js";
+import { Foo } from "./Foo.js";
+
+export const App = {
+  render() {
+    const app = h("div", {}, "App");
+    const foo = h(
+      Foo,
+      {},
+      {
+        handler: ({ age }) => h("p", {}, "handler" + age),
+        footer: () => h("p", {}, "footer"),
+      }
+    );
+    return h("div", {}, [app, foo]);
+  },
+  setup() {
+    return {};
+  },
+};
---
+++ example/componentSlots/Foo.js
@@ -0,0 +1,18 @@
+import { h, renderSlots } from "../../lib/guide-mini-vue.esm.js";
+export const Foo = {
+  setup() {
+    return {};
+  },
+  render() {
+    const foo = h("p", {}, "foo");
+    const age = 18;
+    console.log(this.$slots);
+    return h("div", {}, [
+      renderSlots(this.$slots, "handler", {
+        age,
+      }),
+      foo,
+      renderSlots(this.$slots, "footer"),
+    ]);
+  },
+};
