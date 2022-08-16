350f3914b0024c125023ef97475bb9c5555b9b4a
chenshisand <chenshisand@163.com>
2022-08-17 02:13:29 +0800

    feat:添加diff算法第二节

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -38,18 +38,18 @@ import { ref, h } from "../../lib/guide-mini-vue.esm.js";
 //   h("p", { key: "E" }, "E"),
 // ];
 // 新的比老的长(右)
-const prevChildren = [
-  h("p", { key: "A" }, "A"),
-  h("p", { key: "B" }, "B"),
-  h("p", { key: "C" }, "C"),
-];
-const nextChildren = [
-  h("p", { key: "E" }, "E"),
-  h("p", { key: "D" }, "D"),
-  h("p", { key: "A" }, "A"),
-  h("p", { key: "B" }, "B"),
-  h("p", { key: "C" }, "C"),
-];
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
+// const nextChildren = [
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
 // 老的比新的长(左)
 // const prevChildren = [
 //   h("p", { key: "A" }, "A"),
@@ -74,6 +74,41 @@ const nextChildren = [
 //   h("p", { key: "B" }, "B"),
 //   h("p", { key: "C" }, "C"),
 // ];
+// 对比中间部分
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C", id: "c-prev" }, "C"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "F" }, "F"),
+// ];
+// const nextChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "C", id: "c-next" }, "C"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "F" }, "F"),
+// ];
+// 老的比新的多
+const prevChildren = [
+  h("p", { key: "A" }, "A"),
+  h("p", { key: "B" }, "B"),
+  h("p", { key: "C", id: "c-prev" }, "C"),
+  h("p", { key: "D" }, "D"),
+  h("p", { key: "K" }, "K"),
+  h("p", { key: "E" }, "E"),
+  h("p", { key: "F" }, "F"),
+];
+const nextChildren = [
+  h("p", { key: "A" }, "A"),
+  h("p", { key: "B" }, "B"),
+  h("p", { key: "D" }, "D"),
+  h("p", { key: "C", id: "c-next" }, "C"),
+  h("p", { key: "E" }, "E"),
+  h("p", { key: "F" }, "F"),
+];
 export default {
   name: " ArrayToArray",
   setup() {
